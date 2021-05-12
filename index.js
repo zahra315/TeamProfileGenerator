0808da619ef2c8fe78665b433667a2ba92fc60e5
Zahra Seyedi <za.seyedi@gmail.com>
2021-05-11 23:23:37 -0700

    creat prompt for each role and intialize application

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const path = require("path");
 const { dirname } = require("node:path");
 const fileDirectory = path.resolve(--dirname, 'dist');
 const filePath = path.join(fileDirectory. 'index.html');
-const Engineer = require('./lib/Engineer-class');
-const Intern = require('./lib/Intern-class');
-const Manager = require('./lib/Manager-class');
+const Engineer = require('./lib/EngineerClass');
+const Intern = require('./lib/InternClass');
+const Manager = require('./lib/ManagerClass');
 const Choices = require("inquirer/lib/objects/choices");
 
 let listOfEmployee = [];
 
+// General prompt for employee 
 const prompt = [
     {
         type:"input",
@@ -34,3 +35,77 @@ const prompt = [
         choices:["Manager", "Engineer", "Intern"]
     }
 ]
+
+// Prompt for manager role
+managerPrompt = [
+    {
+        type:"input",
+        name:"officeNumber",
+        message:"What is the office number?",
+        validate: officeNumber => {
+            if(officeNumber) { 
+                return true;
+             } else { 
+                 console.log("This field is Required."); return false;
+             }
+        }
+    }
+]
+
+// Prompt for engineer role
+engineerPrompt = [
+    {
+        type:"input",
+        name:"github",
+        message:"Add link of GitHub URL",
+        validate: github => {
+            if(github) {
+                return true;
+            } else {
+                console.log("This field is Required."); return false;
+            }
+        }
+    }
+]
+
+// Prompt for intern role
+internPrompt = [
+    {
+        type:"input",
+        name:"school",
+        message:"What school is the intern from",
+        validate: school => {
+            if(school) {
+                return true;
+            }else {
+                console.log("This field is Required."); return false;
+            }
+        }
+    }
+]
+
+// Initialize the application 
+const init = () => {
+    if(fs.existsSync(filePath)) {
+        inquirer.prompt({
+            type:"confirm",
+            name:"overwrite",
+            message:"Do you want to overwrite index file?"
+        })
+        .then(async(response) => {
+            let overwrite = response.overwrite;
+            if(await overwrite === true) {
+                console.log("File out requirement");
+                newEmployee();
+            } else{
+                console.log("Redirect current index file before restarting.");
+            }
+        })
+    } else{
+        console.log("File out requirement to generate team profile.");
+        newEmployee();
+    }
+};
+
+
+
